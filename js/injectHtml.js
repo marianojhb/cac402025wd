9fd0c0cfb2eb09f2345eaecfaa90516f3ace2c5f
Mariano Belgrano <mbelgrano@gmail.com>
2025-07-03 13:51:53 -0300

    Agregada funcion injecto

---
+++ js/injectHtml.js
@@ -0,0 +1,20 @@
+// HTML injection
+
+
+
+function injector(url, target) {
+  fetch(url)
+    .then(res => res.text())
+    .then(html => {
+      const elemento = document.querySelector(target);
+      if (elemento) {
+        elemento.insertAdjacentHTML('afterbegin', html);
+      } else {
+        console.warn(`No se encontró el selector: ${target}`);
+      }
+    })
+    .catch(error => {
+      console.error(`Error al inyectar ${url}:`, error);
+    });
+}
+
