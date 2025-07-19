1d3a3805decdab443c7159fc07ae1f94d52b4417
Mariano Belgrano <mbelgrano@gmail.com>
2025-07-19 13:04:49 -0300

    fix3

--- js/conectarBotonShoppingCart.js
+++ js/conectarBotonShoppingCart.js
@@ -3,25 +3,32 @@ export function conectarBotonShoppingCart()
     const shoppingcart = document.getElementById('shoppingcart');
     const divcarrito = document.getElementById('divcarrito');
 
+    if (!shoppingcart || !divcarrito) return;
 
-    if (shoppingcart) {
-        shoppingcart.addEventListener('click', () => {
-            divcarrito.classList.toggle("hidden");
-               divcarrito.classList.toggle("visible");
-        })
-    }
     
+    shoppingcart.addEventListener('click', (event) => {
+        event.stopPropagation();
+        divcarrito.classList.toggle("hidden");
+            divcarrito.classList.toggle("visible");
+    })
+
+    divcarrito.addEventListener('click', (event) => {
+        event.stopPropagation();
+    });
     
     document.addEventListener('click', (event) => {
-        if (divcarrito.classList.contains("visible")) {
-            if ( !divcarrito.contains(event.target) && !shoppingcart.contains(event.target)) {
-                divcarrito.classList.add("hidden");
-                divcarrito.classList.remove("visible");    
+
+            if (divcarrito.classList.contains("visible")) 
+            {
+                if ( !divcarrito.contains(event.target) && 
+                    !shoppingcart.contains(event.target) ) 
+                {
+                    divcarrito.classList.add("hidden");
+                    divcarrito.classList.remove("visible");    
+                }
                 
-              
             }
-            
-        }
+
     });
 
 }
